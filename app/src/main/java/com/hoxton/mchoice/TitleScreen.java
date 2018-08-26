00c7384e35335b5098e5ce8ce4283102df7ea8e4
Alan <37944809+Starscream29@users.noreply.github.com>
2018-08-26 09:09:59 -0600

    Version 0
    Initial Commit

---
+++ app/src/main/java/com/hoxton/mchoice/TitleScreen.java
@@ -0,0 +1,13 @@
+package com.hoxton.mchoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
